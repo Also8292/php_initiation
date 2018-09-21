f9c9d743ea5cd369dee81f19202bd9855e622903
Also8292 <sowalils1992@gmail.com>
2018-09-21 10:38:36 +0000

    exercice 1 sans redirection2

--- projet_multiplications-master/Niveau 1/post.php
+++ projet_multiplications-master/Niveau 1/post.php
@@ -1,17 +1,9 @@
 <?php
     
-    if(isset($_GET['table'])) {
-        echo 'Table de ' . $_GET['table'] .PHP_EOL .PHP_EOL;
-        for($i = 0 ; $i <=10 ; $i++) {
-            $y = $_GET['table'];
-            echo $y . ' x ' . $i . ' = ' . $y * $i .PHP_EOL;
-        }
-    }
-    else {
-        $y = 0;
-        for($i = 0 ; $i <=10 ; $i++) {
-            echo $y . ' x ' . $i . ' = ' . $y * $i .PHP_EOL;
-        }
+    echo 'Table de ' . $_GET['table'] .PHP_EOL .PHP_EOL;
+    for($i = 0 ; $i <=10 ; $i++) {
+        $y = $_GET['table'];
+        echo $y . ' x ' . $i . ' = ' . $y * $i .PHP_EOL;
     }
     
 ?>
